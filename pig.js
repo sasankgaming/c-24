9e47192b9684bb67f2f461f4578f9f42d10695fa
sasankgaming <70948071+sasankgaming@users.noreply.github.com>
2020-09-18 15:57:14 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,35 @@
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.7,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y,40,40, options);
+      this.width = 40;
+      this.height = 40;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("pink");
+      strokeWeight(2);
+      stroke("orange");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+
+
+
+
+
+
+
+
